bca6083ac48247760016f92e6a22dbdbb11b72e2
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-12-09 08:17:23 +0600

    Auth Layout Created

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router';
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/HomePage/Home';
+import AuthLayout from '@/layouts/AuthLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
     Component: RootLayout,
     children: [{ index: true, Component: Home }],
+  },
+  {
+    path: '/auth',
+    Component: AuthLayout,
   },
 ]);
 
