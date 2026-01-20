0bfd9effcd22be861dc2afbc33210f31344f591c
tashinmustakim-39 <tashinmustakim@iut-dhaka.edu>
2026-01-20 08:29:32 +0600

    Fixed Blog Layout.jsx

--- src/layouts/BlogLayout.jsx
+++ src/layouts/BlogLayout.jsx
@@ -9,6 +9,9 @@ const BlogLayout = () => {
         <nav className="sticky top-0 z-100">
           <Navbar />
         </nav>
+        <main className="min-h-[calc(100vh-40px)]">
+          <Outlet />
+        </main>
         <Footer />
       </div>
     </>
